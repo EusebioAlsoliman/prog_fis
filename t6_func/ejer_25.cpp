19a83c4d1fca88a9b02f487b7d7c4c017b642662
EusebioAlSoliman <eusebio.alsoliman@gmail.com>
2024-10-19 14:36:53 +0200

    Mejora en ejercicio 25

--- t6_func/ejer_25.cpp
+++ t6_func/ejer_25.cpp
@@ -3,10 +3,10 @@
 
 using namespace std;
 
-double funcion(int n, double x)
+float funcion(int n, float x)
 {
     int termino;
-    double suma = 0.0;
+    float suma = 0.0;
     for(int i = 0; i <= n; i++){
         termino = 2*i + 1;
         suma = suma + ((pow(x,termino))/termino);
@@ -18,17 +18,19 @@ double funcion(int n, double x)
 int main() {
     // Declaración de variables
     int n;
-    double x;
+    float x;
 
-    // Se pide al usuario que introduzca el valor de n y x
-    cout << "Introduzca el valor de x: " << endl;
-    cin >> x;
+    do {
+        // Se pide al usuario que introduzca el valor de n y x
+        cout << "Introduzca el valor de x (debe ser menor a la unidad): " << endl;
+        cin >> x;
+    } while (x >= 1);
 
     cout << "Introduzca el valor de n: " << endl;
     cin >> n;
 
     // Llamada a la función y se guarda resultado en la variable 'resultado'
-    double resultado = funcion(n, x);
+    float resultado = funcion(n, x);
 
     cout << "El resultado es: " << resultado << endl;
 
